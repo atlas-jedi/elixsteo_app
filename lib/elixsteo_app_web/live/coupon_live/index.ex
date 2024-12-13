ee923d653c25a249bb98b7f723f7c071ed09380a
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-12 22:06:05 -0300

    feat: stream coupons and display in a table

--- lib/elixsteo_app_web/live/coupon_live/index.ex
+++ lib/elixsteo_app_web/live/coupon_live/index.ex
@@ -1,8 +1,10 @@
 defmodule ElixsteoAppWeb.CouponLive.Index do
   use ElixsteoAppWeb, :live_view
 
+  alias ElixsteoApp.Coupons
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, stream(socket, :coupons, Coupons.list_coupons())}
   end
 end
