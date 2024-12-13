b7d5bc873dcdea887dc613a90bdae15ec756ccad
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-13 18:20:56 -0300

    feat: update coupon display and seed sample data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ElixsteoApp.Repo
+alias ElixsteoApp.Coupons.Coupon
+
+# Clear existing coupons
+Repo.delete_all(Coupon)
+
+# Create sample coupons
+[
+  %{
+    name: "WELCOME10",
+    status: "active",
+    discount: "10.0",
+    type: "percentage"
+  },
+  %{
+    name: "SAVE20",
+    status: "active",
+    discount: "20.0",
+    type: "percentage"
+  },
+  %{
+    name: "FIXED50",
+    status: "active",
+    discount: "50.0",
+    type: "fixed"
+  },
+  %{
+    name: "SUMMER2024",
+    status: "inactive",
+    discount: "15.0",
+    type: "percentage"
+  }
+]
+|> Enum.each(fn coupon_data ->
+  %Coupon{}
+  |> Coupon.changeset(coupon_data)
+  |> Repo.insert!()
+end)
