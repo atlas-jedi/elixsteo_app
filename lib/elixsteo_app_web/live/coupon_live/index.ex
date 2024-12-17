a9c7ec164345aea841d24ee2ca81f9940e750d00
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-16 23:09:08 -0300

    feat: implement delete confirmation modal

--- lib/elixsteo_app_web/live/coupon_live/index.ex
+++ lib/elixsteo_app_web/live/coupon_live/index.ex
@@ -5,6 +5,39 @@ defmodule ElixsteoAppWeb.CouponLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, stream(socket, :coupons, Coupons.list_coupons())}
+    {:ok,
+     socket
+     |> stream(:coupons, Coupons.list_coupons())
+     |> assign(:delete_modal_open, false)
+     |> assign(:coupon_to_delete, nil)}
+  end
+
+  @impl true
+  def handle_event("open_delete_modal", %{"id" => id}, socket) do
+    {:noreply,
+     socket
+     |> assign(:delete_modal_open, true)
+     |> assign(:coupon_to_delete, id)}
+  end
+
+  @impl true
+  def handle_event("close_delete_modal", _params, socket) do
+    {:noreply,
+     socket
+     |> assign(:delete_modal_open, false)
+     |> assign(:coupon_to_delete, nil)}
+  end
+
+  @impl true
+  def handle_event("delete_coupon", %{"id" => id}, socket) do
+    coupon = Coupons.get_coupon!(id)
+    {:ok, _} = Coupons.delete_coupon(coupon)
+
+    {:noreply,
+     socket
+     |> stream_delete(:coupons, coupon)
+     |> assign(:delete_modal_open, false)
+     |> assign(:coupon_to_delete, nil)
+     |> put_flash(:info, "Cupom excluído com sucesso")}
   end
 end
