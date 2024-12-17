65521f6cfae1d1a296b32caf8598d24f26169bca
André Pestana Silva <atlas.ifpa@gmail.com>
2024-12-16 23:10:02 -0300

    chore: add Dialyxir

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule ElixsteoApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
